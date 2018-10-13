044699fc18bca8470e571f74727479714297e284
Khaled Karman <k@rawasy.com>
2018-10-12 22:26:28 -0700

    D3 HW

--- UFO_Sightings/D3/assets/js/app.js
+++ UFO_Sightings/D3/assets/js/app.js
@@ -38,7 +38,7 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Retrieve data from the CSV file and execute everything below
-d3.csv("../assets/data/data.csv").then(function(census, err){
+d3.csv("../data/data.csv").then(function(census, err){
   if (err) throw err;
 
   // parse data
